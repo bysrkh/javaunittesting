b163fab8c0d8e330a6993d78d46d86d4550a2bd1
esarijal <esa.rijal@gmail.com>
2018-11-06 14:24:20 +0700

    Using More Refactored Mockito

--- src/test/java/com/mitrais/unittesting/business/SomeBusinessImplMockTest.java
+++ src/test/java/com/mitrais/unittesting/business/SomeBusinessImplMockTest.java
@@ -1,20 +1,22 @@
 package com.mitrais.unittesting.business;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SomeBusinessImplMockTest {
 
-    private SomeDataService dataServiceMock = mock(SomeDataService.class);;
-    private SomeBusinessImpl someBusiness = new SomeBusinessImpl();
-
-    @Before
-    public void setUp() throws Exception {
-        someBusiness.setSomeDataService(dataServiceMock);    }
+    @InjectMocks
+    private SomeBusinessImpl someBusiness;
+    @Mock
+    private SomeDataService dataServiceMock;
 
     @Test
     public void calculateSumUsingDataService_Basic(){
