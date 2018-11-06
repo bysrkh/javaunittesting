44e495040a30e1f9d99af0dcccf04d58e00139c9
esarijal <esa.rijal@gmail.com>
2018-11-06 14:40:58 +0700

    Mockito Multiple return values

--- src/test/java/com/mitrais/unittesting/business/ListMockTest.java
+++ src/test/java/com/mitrais/unittesting/business/ListMockTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -30,5 +32,13 @@ public class ListMockTest {
     public void returnWithParameters(){
         when(mock.get(0)).thenReturn("javaBootcamp");
         assertEquals("javaBootcamp", mock.get(0));
+        assertEquals(null, mock.get(1));
+    }
+
+    @Test
+    public void returnWithGenericParameters(){
+        when(mock.get(anyInt())).thenReturn("javaBootcamp");
+        assertEquals("javaBootcamp", mock.get(0));
+        assertEquals("javaBootcamp", mock.get(1));
     }
 }
