6c490480ce62f12abde0e8e98014569efbae91e7
esarijal <esa.rijal@gmail.com>
2018-11-06 14:32:25 +0700

    Mockito Multiple return values

---
+++ src/test/java/com/mitrais/unittesting/business/ListMockTest.java
@@ -0,0 +1,34 @@
+package com.mitrais.unittesting.business;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListMockTest {
+
+    List mock = mock(List.class);
+
+    @Test
+    public void size_Basic(){
+        when(mock.size()).thenReturn(5);
+        assertEquals(mock.size(), 5);
+    }
+
+    @Test
+    public void returnDifferentValues(){
+
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParameters(){
+        when(mock.get(0)).thenReturn("javaBootcamp");
+        assertEquals("javaBootcamp", mock.get(0));
+    }
+}
