89c4426089d84062e62aee570c70f9558d86b734
esarijal <esa.rijal@gmail.com>
2018-11-07 08:45:19 +0700

    Testing Web Layer (Controller) again

--- src/test/java/com/mitrais/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/mitrais/unittesting/controller/ItemControllerTest.java
@@ -15,6 +15,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -54,6 +56,32 @@ public class ItemControllerTest {
         mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().json("{id:2,name:Item2,price:25000,quantity:9}"))
+                .andReturn();
+    }
+
+    @Test
+    public void retrieveAllItems_basic() throws Exception {
+        when(itemBusinessService.retrieveAllItems())
+                .thenReturn(
+                        Arrays.asList(
+                                new Item(1, "Item1", 15000, 10),
+                                new Item(2, "Item2", 25000, 20),
+                                new Item(3, "Item3", 35000, 30)
+                        )
+                );
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/all-items-from-database")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        "[" +
+                                        "{id:1,name:Item1,price:15000,quantity:10}," +
+                                        "{id:2,name:Item2,price:25000,quantity:20}," +
+                                        "{id:3,name:Item3,price:35000,quantity:30}" +
+                                    "]"))
                 .andReturn();
     }
 }
