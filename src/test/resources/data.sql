3398da8d7abdcf4a9154235482cf8b9c69eda457
esarijal <esa.rijal@gmail.com>
2018-11-07 09:17:14 +0700

    Unit Test for JPA Repository

---
+++ src/test/resources/data.sql
@@ -0,0 +1,10 @@
+-- insert into item values(11, 'Item 1', 1000, 15);
+--
+-- insert into item values(12, 'Item 2', 1300, 11);
+--
+-- insert into item values(13, 'Item 3', 1100, 12);
+
+insert into item values(14, 'Item 4', 1400, 14);
+
+insert into item values(15, 'Item 5', 1500, 17);
+
